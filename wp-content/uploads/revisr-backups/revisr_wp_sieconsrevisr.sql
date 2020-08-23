8ff1d97e4f0c83291a1cb8edb2a8e0ca8184a7ce
ernesto <david.montano.rosabal@gmail.com>
2020-08-23 23:48:54 +0200

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_sieconsrevisr.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_sieconsrevisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `wp_sieconsrevisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_sieconsrevisr` WRITE;
 /*!40000 ALTER TABLE `wp_sieconsrevisr` DISABLE KEYS */;
-INSERT INTO `wp_sieconsrevisr` VALUES (1,'2020-08-23 20:15:08','Error pulling changes from the remote repository.','error','ernesto'),(2,'2020-08-23 20:17:57','Discarded all uncommitted changes.','discard','ernesto'),(3,'2020-08-23 20:18:15','Error pulling changes from the remote repository.','error','ernesto'),(4,'2020-08-23 20:45:35','Error pulling changes from the remote repository.','error','ernesto'),(5,'2020-08-23 20:49:04','Error pulling changes from the remote repository.','error','ernesto'),(6,'2020-08-23 20:49:58','Error pulling changes from the remote repository.','error','ernesto'),(7,'2020-08-23 20:54:26','Created new branch: ernesto','branch','ernesto'),(8,'2020-08-23 20:54:26','Checked out branch: ernesto.','branch','ernesto'),(9,'2020-08-23 20:54:37','Error pulling changes from the remote repository.','error','ernesto'),(10,'2020-08-23 20:55:50','Error pulling changes from the remote repository.','error','ernesto'),(11,'2020-08-23 21:19:37','Error pulling changes from the remote repository.','error','ernesto');
+INSERT INTO `wp_sieconsrevisr` VALUES (1,'2020-08-23 20:15:08','Error pulling changes from the remote repository.','error','ernesto'),(2,'2020-08-23 20:17:57','Discarded all uncommitted changes.','discard','ernesto'),(3,'2020-08-23 20:18:15','Error pulling changes from the remote repository.','error','ernesto'),(4,'2020-08-23 20:45:35','Error pulling changes from the remote repository.','error','ernesto'),(5,'2020-08-23 20:49:04','Error pulling changes from the remote repository.','error','ernesto'),(6,'2020-08-23 20:49:58','Error pulling changes from the remote repository.','error','ernesto'),(7,'2020-08-23 20:54:26','Created new branch: ernesto','branch','ernesto'),(8,'2020-08-23 20:54:26','Checked out branch: ernesto.','branch','ernesto'),(9,'2020-08-23 20:54:37','Error pulling changes from the remote repository.','error','ernesto'),(10,'2020-08-23 20:55:50','Error pulling changes from the remote repository.','error','ernesto'),(11,'2020-08-23 21:19:37','Error pulling changes from the remote repository.','error','ernesto'),(12,'2020-08-23 21:31:26','Successfully backed up the database.','backup','ernesto'),(13,'2020-08-23 21:31:26','Error pulling changes from the remote repository.','error','ernesto');
 /*!40000 ALTER TABLE `wp_sieconsrevisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
