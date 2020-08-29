baa8728306b847d63ea3a831f34036826ed64339
ernesto <david.montano.rosabal@gmail.com>
2020-08-30 00:44:30 +0200

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_sieconsyoast_indexable_hierarchy.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_sieconsyoast_indexable_hierarchy.sql
@@ -1,17 +1,4 @@
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wp_sieconsyoast_indexable_hierarchy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wp_sieconsyoast_indexable_hierarchy` (
   `indexable_id` int(11) unsigned NOT NULL,
   `ancestor_id` int(11) unsigned NOT NULL,
@@ -22,20 +9,6 @@ CREATE TABLE `wp_sieconsyoast_indexable_hierarchy` (
   KEY `ancestor_id` (`ancestor_id`),
   KEY `depth` (`depth`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 LOCK TABLES `wp_sieconsyoast_indexable_hierarchy` WRITE;
-/*!40000 ALTER TABLE `wp_sieconsyoast_indexable_hierarchy` DISABLE KEYS */;
-INSERT INTO `wp_sieconsyoast_indexable_hierarchy` VALUES (10,3,1,1),(15,3,1,1),(16,3,1,1),(17,3,1,1),(18,3,1,1),(19,3,1,1),(20,3,1,1),(34,3,1,1),(45,7,1,1),(46,7,1,1),(48,7,1,1),(54,7,1,1),(55,7,1,1),(66,61,1,1),(107,8,1,1),(111,11,1,1),(112,11,1,1),(113,11,1,1),(114,11,1,1),(115,11,1,1),(128,6,1,1),(129,6,1,1),(130,6,1,1),(131,6,1,1),(133,6,1,1),(134,7,1,1),(137,8,1,1),(138,6,1,1),(140,24,1,1),(160,11,1,1);
-/*!40000 ALTER TABLE `wp_sieconsyoast_indexable_hierarchy` ENABLE KEYS */;
+INSERT INTO `wp_sieconsyoast_indexable_hierarchy` VALUES ('10','3','1','1'), ('15','3','1','1'), ('16','3','1','1'), ('17','3','1','1'), ('18','3','1','1'), ('19','3','1','1'), ('20','3','1','1'), ('34','3','1','1'), ('45','7','1','1'), ('46','7','1','1'), ('48','7','1','1'), ('54','7','1','1'), ('55','7','1','1'), ('66','61','1','1'), ('107','8','1','1'), ('111','11','1','1'), ('112','11','1','1'), ('113','11','1','1'), ('114','11','1','1'), ('115','11','1','1'), ('128','6','1','1'), ('129','6','1','1'), ('130','6','1','1'), ('131','6','1','1'), ('133','6','1','1'), ('134','7','1','1'), ('137','8','1','1'), ('138','6','1','1'), ('140','24','1','1'), ('160','11','1','1');
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
