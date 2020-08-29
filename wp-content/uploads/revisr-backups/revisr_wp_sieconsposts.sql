56f1575bf85ba8e3d98f5e72408e837fefded991
ernesto <david.montano.rosabal@gmail.com>
2020-08-30 01:01:06 +0200

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_sieconsposts.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_sieconsposts.sql
@@ -1,4 +1,17 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wp_sieconsposts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wp_sieconsposts` (
   `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `post_author` bigint(20) unsigned NOT NULL DEFAULT 0,
@@ -29,6 +42,20 @@ CREATE TABLE `wp_sieconsposts` (
   KEY `post_parent` (`post_parent`),
   KEY `post_author` (`post_author`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1181 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 LOCK TABLES `wp_sieconsposts` WRITE;
-INSERT INTO `wp_sieconsposts` VALUES ('1180','1','2020-08-30 09:55:41','0000-00-00 00:00:00','','Borrador automático','','auto-draft','closed','open','','','','','2020-08-30 09:55:41','0000-00-00 00:00:00','','0','http://localhost/siecons/?p=1180','0','post','','0');
+/*!40000 ALTER TABLE `wp_sieconsposts` DISABLE KEYS */;
+INSERT INTO `wp_sieconsposts` VALUES (1180,1,'2020-08-30 09:55:41','0000-00-00 00:00:00','','Borrador automático','','auto-draft','closed','open','','','','','2020-08-30 09:55:41','0000-00-00 00:00:00','',0,'http://localhost/siecons/?p=1180',0,'post','',0);
+/*!40000 ALTER TABLE `wp_sieconsposts` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
