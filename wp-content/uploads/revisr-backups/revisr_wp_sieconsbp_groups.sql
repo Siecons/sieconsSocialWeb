b21728243886e2fe0cd92a2d3c73cc69cdf777b5
ernesto <david.montano.rosabal@gmail.com>
2020-09-09 23:30:40 +0200

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_sieconsbp_groups.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_sieconsbp_groups.sql
@@ -1,4 +1,17 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wp_sieconsbp_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wp_sieconsbp_groups` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `creator_id` bigint(20) NOT NULL,
@@ -14,6 +27,20 @@ CREATE TABLE `wp_sieconsbp_groups` (
   KEY `status` (`status`),
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 LOCK TABLES `wp_sieconsbp_groups` WRITE;
-INSERT INTO `wp_sieconsbp_groups` VALUES ('1','1','Comunidad','comunidad','Este sera el grupo general al que perteneceran todos los usuarios','public','0','0','2020-06-24 17:17:47');
+/*!40000 ALTER TABLE `wp_sieconsbp_groups` DISABLE KEYS */;
+INSERT INTO `wp_sieconsbp_groups` VALUES (1,1,'Comunidad','comunidad','Este sera el grupo general al que perteneceran todos los usuarios','public',0,0,'2020-06-24 17:17:47');
+/*!40000 ALTER TABLE `wp_sieconsbp_groups` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
--- wp-content/uploads/revisr-backups/revisr_wp_sieconsyoast_primary_term.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_sieconsyoast_primary_term.sql
@@ -1,4 +1,17 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wp_sieconsyoast_primary_term`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wp_sieconsyoast_primary_term` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(11) unsigned NOT NULL,
@@ -11,6 +24,20 @@ CREATE TABLE `wp_sieconsyoast_primary_term` (
   KEY `post_taxonomy` (`post_id`,`taxonomy`),
   KEY `post_term` (`post_id`,`term_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 LOCK TABLES `wp_sieconsyoast_primary_term` WRITE;
-INSERT INTO `wp_sieconsyoast_primary_term` VALUES ('1','242','1','category','2020-06-22 21:42:03','2020-07-10 20:46:15','1');
+/*!40000 ALTER TABLE `wp_sieconsyoast_primary_term` DISABLE KEYS */;
+INSERT INTO `wp_sieconsyoast_primary_term` VALUES (1,242,1,'category','2020-06-22 21:42:03','2020-07-10 18:46:15',1);
+/*!40000 ALTER TABLE `wp_sieconsyoast_primary_term` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
